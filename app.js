88892e708c07d0fdb8c849e5e872ec61c2f206d3
SScript <sscriptnet@gmail.com>
2017-04-03 22:43:52 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+    var express = require('express')
+    var app = express()
+
+    app.get('/', function(req, res) {
+      res.send('Hello world!')
+    })
+
+    app.get('/:time', function(req, res) {
+
+      var timeVar = req.params.time;
+      var date;
+
+      var formatting = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      };
+
+      if (isNaN(timeVar)) {
+
+        date = timeVar;
+        timeVar = new Date(timeVar) / 1000;
+
+        if (isNaN(timeVar)) 
+          date = null;
+
+      } else {
+
+      
+        date = new Date(timeVar * 1000);
+        date = date.toLocaleDateString("en-eu", formatting);
+
+        timeVar = Number(timeVar);
+
+
+
+      }
+
+      res.json({unixtime: timeVar, natural: date})
+
+
+    })
+
+    app.listen(3000, function(err, success) {
+      if (err) return err;
+      console.log("Connected to port 3000");
+    });
